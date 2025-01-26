07a90b0af430b6e761f8f6f657e050cdb6eec87a
Deshan-AI <deshantharaka422@gmail.com>
2025-01-26 11:30:56 +0530

    school

---
+++ src/main/java/com/wildlifebackend/wildlife/entitiy/Student.java
@@ -0,0 +1,81 @@
+package com.wildlifebackend.wildlife.entitiy;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+
+@Entity
+@Setter
+@Getter
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private LocalDate dateOfBirth;
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getAge(){
+        return Period.between(this.dateOfBirth,LocalDate.now()).getYears();
+    }
+}
